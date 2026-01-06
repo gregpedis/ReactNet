88107f7018ad740fdc90ccd31fc3ad8d34f7b3ed
gregpedis <trafalgar1618@hotmail.com>
2026-01-06 19:06:10 +0100

    UseState

--- BaseComponent.cs
+++ BaseComponent.cs
@@ -15,7 +15,26 @@ abstract class BaseComponent
 	protected (T state, Action<T> setState) UseState<T>(T initialValue)
 	{
 		ColorConsole.WriteLineState("UseState called.");
-		return (initialValue, default);
+
+		var currentIndex = _hookIndex;
+		_hookIndex++;
+
+		if (currentIndex >= _hooks.Count)
+		{
+			ColorConsole.WriteLineState($"First time seeing this hook called. Initializing it with: [{initialValue}].");
+			_hooks.Add(new UseStateHookData<T>(initialValue));
+		}
+
+		Action<T> setState = (newValue) =>
+		{
+			ColorConsole.WriteLineState($"Time for some GOTOs. Let's go to the beginning after setting whatever this is to [{newValue}].");
+			_hooks[currentIndex] = new UseStateHookData<T>(newValue);
+			ForceRender();
+		};
+
+		ColorConsole.WriteLineState($"Returning from UseState, giving the user the current value and an over-engineered mental asylum setter.");
+		var state = (_hooks[currentIndex] as UseStateHookData<T>).Value;
+		return (state, setState);
 	}
 
 
